70f71ad725aa2929cb1abc37a25a3f6ee733c74b
SOFT\d.burov <d.burov@itavia.ru>
2019-08-19 19:53:45 +0300

    add unit stats view

---
+++ ProjectHandbook/ProjectHandbook/Views/UnitView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectHandbook.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UnitView : ContentView
+    {
+        public UnitView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
